87a5b91497dabed5c4f9f9a63690245a387f35e1
k-p-jones <kenjones620@yahoo.co.uk>
2020-07-04 19:43:24 +0100

    Create dashboard controller and view, set it as user route path Use css animations to dismiss notices

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'dashboard' => 'dashboard#index', :as => :user_root
   root 'home#index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -9,26 +9,27 @@ RSpec.describe 'User sign in', type: :feature do
 
   context 'with a valid user account' do
     it 'logs the user in and enables them to log out' do
-      # This should redirect to the posts/dashboard path
-      # when those routes are added.
-      visit '/users/sign_in'
+      visit user_session_path
       expect(page).to have_selector(:link_or_button, 'Log In')
       log_in_with(user.email, user.password)
+      # successful login should redirect to the user root path
       expect(page).to have_content('Signed in successfully.')
-      expect(current_path).to eql(root_path)
+      expect(current_path).to eql(user_root_path)
       expect(page).to have_selector(:link_or_button, 'Log Out')
-
+      # logout should redirect to the root path
       click_link 'Log Out'
+      expect(current_path).to eql(root_path)
       expect(page).to have_content('Signed out successfully.')
     end
   end
 
   context 'without a valid user account' do
     it 'does not allow sign in' do
-      visit '/users/sign_in'
+      visit user_session_path
       expect(page).to have_selector(:link_or_button, 'Log In')
       log_in_with(invalid_email, invalid_password)
       expect(page).to have_content('Invalid Email or password.')
+      expect(current_path).to eql(user_session_path)
     end
   end
 end
