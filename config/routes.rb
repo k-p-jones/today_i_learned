28c0c0cc917136618309ccc4abf333406ffbac4b
k-p-jones <kenjones620@yahoo.co.uk>
2020-07-05 10:42:01 +0100

    Add routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'dashboard' => 'dashboard#index', :as => :user_root
   root 'home#index'
   devise_for :users
+
+  get 'dashboard' => 'dashboard#index', :as => :user_root
+  resources :posts, except: [:index]
 end
