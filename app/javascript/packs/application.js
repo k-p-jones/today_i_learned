a6f97a4b6ae4bc6c92d4c1a86e5ee18382b48bdd
k-p-jones <kenjones620@yahoo.co.uk>
2020-07-13 17:08:27 +0100

    Tidy scss files, extract file upload override to its own file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,31 +2,15 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//trix editor
+import 'trix/dist/trix.css';
+import 'trix/dist/trix.js';
+import 'src/js/highlight';
+import 'src/js/disable-file-uploads';
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-//trix editor
-import 'trix/dist/trix.css';
-import 'trix/dist/trix.js';
-
 require("@rails/actiontext")
-require("styles/application.css")
-
-import 'src/js/highlight';
-
-// Prevent image attachments in posts
-window.addEventListener("trix-file-accept", function(event) {
-  event.preventDefault();
-  alert("File attachment not supported!");
-});
+require("styles/application.scss")
