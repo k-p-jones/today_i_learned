a4939cc073e7ba7dbf8dcf983804aeefc42130f4
k-p-jones <kenjones620@yahoo.co.uk>
2020-07-16 19:01:34 +0100

    Remove view button from home page, turn post titles into links. Update specs.

--- spec/features/user_dashboard_spec.rb
+++ spec/features/user_dashboard_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'User Dashboard', type: :feature do
     it 'lets a user view the dashboard' do
       log_in_with(user.email, user.password)
       expect(current_path).to eql(user_root_path)
-      expect(page).to have_content(post.title)
+      expect(page).to have_link(post.title, href: post_path(post.id))
       expect(page).to have_link('View', href: post_path(post.id))
       expect(page).to have_link('Edit', href: edit_post_path(post.id))
       expect(page).to have_link('Destroy', href: post_path(post.id))
